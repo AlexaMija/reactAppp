834c2c78e8f8fcf218dddc480e91276793c1be84
Alexa Mija <aleksandragmitrovic17@gmail.com>
2020-08-06 13:37:06 +0200

    Created files
    Description

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+import React from "react"
+import ReactDOM from "react-dom"
+function Footer(props) {
+  return(
+    <div>
+    <p>Napisi tri poruke koje te motivisu da ostvaris svoj cilj!</p>
+    <h3>
+  <input type="text"/>
+    </h3>
+    <h3>
+ <input type="text"/>
+    </h3>
+    <h3>
+ <input type="text"/>
+    </h3>
+    <h3>
+ <input type="text"/>
+    </h3>
+      </div>
+  )
+}
+
+export default Footer
---
+++ src/MainContent.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+import ReactDOM from "react-dom"
+function MainContent () {
+  return(
+<div>
+<h3>This is Main</h3>
+</div>
+
+  )
+}
+export default MainContent
